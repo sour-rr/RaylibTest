0589c933cd49813db27feeaf3010f27bcd8f0862
sour-rr <210703962+sour-rr@users.noreply.github.com>
2025-11-12 01:24:22 +0000

    Changes for Sphere from main to camera-system-v2

--- src/Sphere.cs
+++ src/Sphere.cs
@@ -1,11 +1,12 @@
 using Raylib_cs;
 using System;
+using System.Drawing;
 using System.Numerics;
 
 public class Sphere : Mesh
 {
-    public int StepX; //20
-    public int StepY; //15
+    public int StepX;
+    public int StepY;
     public int Radius;
     public Sphere(string name, int verticeCount, int faceCount, Vector3 pos, int stepCountX, int stepCountY, int radius) : base(name, verticeCount, faceCount, pos)
     {
@@ -16,9 +17,8 @@ public class Sphere : Mesh
 
     public void CreateSphere()
     {
-        int count = 0;
         for (int i = 0; i <= 180; i += StepY) //latitude 
-            for (int j = 0; j < 360; j += StepX)
+            for (int j = 0; j <= 360; j += StepX)
             {
                 double phi = Math.PI * i / 180;
                 double theta = Math.PI * j / 180;
@@ -26,13 +26,27 @@ public class Sphere : Mesh
                 int y = (int)(Radius * Math.Cos(phi));
                 int z = (int)(Radius * Math.Sin(phi) * Math.Sin(theta));
                 AddVertex(new(x, y, z));
-                count++;
             }
-        Console.WriteLine($"Vertex Count: {count}");
-    }
 
-    private void CreateWireFrame()
+        AddFaces();
+    }
+    
+    private void AddFaces()
     {
-        
-    }   
+        //retrieve the vectors from the array of vertices to make up the faces of the triangle
+        int xPoint = StepX / 360;
+        int yPoint = (StepY / 180) + 1;
+        for(int i = 0; i < yPoint; i++)
+            for(int j = 0; j < xPoint; j++)
+            {
+                int point1 = (j % xPoint) + (xPoint * i);
+                int point2 = ((j + 1) % xPoint) + (xPoint * i);
+                int point3 = (j % xPoint) + (xPoint * (i + 1));
+                int point4 = ((j + 1) % xPoint) + (xPoint * (i + 1));
+
+                AddFace(point1, point2, point3);
+                AddFace(point3, point4, point2);
+                Console.WriteLine($"{point1} {point2} {point3} {point4}");
+            }
+    }
 }
