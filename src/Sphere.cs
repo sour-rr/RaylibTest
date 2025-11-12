47c993b0281792c2bc8e521cf6dbf108b5e03f33
sour-rr <210703962+sour-rr@users.noreply.github.com>
2025-11-12 01:11:36 +0000

    Created Sphere Wire Mesh algorithm to label the points that make up the faces of the triangle on the sphere

--- src/Sphere.cs
+++ src/Sphere.cs
@@ -16,7 +16,7 @@ public class Sphere : Mesh
 
     public void CreateSphere()
     {
-        for(int i = 0; i <= 180; i += StepY) //latitude 
+        for (int i = 0; i <= 180; i += StepY) //latitude 
             for (int j = 0; j <= 360; j += StepX)
             {
                 double phi = Math.PI * i / 180;
@@ -25,6 +25,26 @@ public class Sphere : Mesh
                 int y = (int)(Radius * Math.Cos(phi));
                 int z = (int)(Radius * Math.Sin(phi) * Math.Sin(theta));
                 AddVertex(new(x, y, z));
+            }
+
+        AddFaces();
+    }
+    
+    private void AddFaces()
+    {
+        //retrieve the vectors from the array of vertices to make up the faces of the triangle
+        int xPoint = StepX / 360;
+        int yPoint = (StepY / 180) + 1;
+        for(int i = 0; i < yPoint; i++)
+            for(int j = 0; j < xPoint; j++)
+            {
+                int point1 = (j % xPoint) + (xPoint * i);
+                int point2 = ((j + 1) % xPoint) + (xPoint * i);
+                int point3 = (j % xPoint) + (xPoint * (i + 1));
+                int point4 = ((j + 1) % xPoint) + (xPoint * (i + 1));
+
+                AddFace(point1, point2, point3);
+                AddFace(point3, point4, point2);
             }
     }
 }
